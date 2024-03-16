a7836947cf80d79fe19326b2adf6ebb73abebf3a
NevilleJS <69287021+s3dman@users.noreply.github.com>
2024-03-16 08:32:20 +0530

    backed

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,24 @@
 import React from "react";
+import { deploy, donate, balance, trans } from "@/lib/block";
 
 async function Page() {
+	// const x = await deploy(
+	// 	"0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d",
+	// 	[
+	// 		"0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+	// 		"0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+	// 	],
+	// );
+	// const y = await donate(
+	// 	"0x948b3c65b89df0b4894abe91e6d02fe579834f8f",
+	// 	"0x8b3a350cf5c34c9194ca85829a2df0ec3153be0318b5e2d3348e872092edffba",
+	// 	[100, 0],
+	// 	"10",
+	// );
+	// const z = await balance("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
+	// const u = await trans( "0xa97e205444a5cb08356d3cd7b4a1e39cc49855baca94f9b85fb04f8548737cf0",);
+	// console.log(u);
+
 	return <>mainpage</>;
 }
 
